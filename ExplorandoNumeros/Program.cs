6bfe6594f11342b3ee6d5c4f9d276769ad077b2d
miguegta <miguelgaitan4@gmail.com>
2024-05-15 15:59:50 -0600

    first commit

---
+++ ExplorandoNumeros/Program.cs
@@ -0,0 +1,13 @@
+﻿// Número >= 0
+uint numeroNatural = 23;
+
+// N Positivo o Negativo
+int numero = -30;
+
+// Declarando
+int n;
+n = 10; // Asignar
+
+// Declarar e inicializar
+int n2 = 20;
+n2 = 30; //Asignando un nuevo valor
---
+++ VariablesTexto/Program.cs
@@ -0,0 +1,26 @@
+﻿// Tipo char
+
+char letra = 'A';
+char digito = '1';
+char simbolo = '$';
+
+Console.WriteLine(letra);
+Console.WriteLine(digito);
+Console.WriteLine(simbolo);
+
+Console.WriteLine("");
+
+// Tipo string
+
+string nombre = "Migue";
+string apellido = "Gaitán";
+string telefono = "(502) 4244-6037";
+string nombreApellido = "Migue\nGaitán";
+
+Console.WriteLine(nombre);
+Console.WriteLine(apellido);
+Console.WriteLine(telefono);
+Console.WriteLine(nombreApellido);
+
+
+
