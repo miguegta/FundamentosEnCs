0bca19b77284949ecc61255b23dc9a310699127d
miguegta <miguelgaitan4@gmail.com>
2024-05-23 21:59:22 -0600

    first commit

---
+++ OperadorIncremento/Program.cs
@@ -0,0 +1,35 @@
+﻿// Posfijo
+int edad = 10;
+Console.WriteLine(edad);
+edad++;
+Console.WriteLine(edad);
+edad--;
+Console.WriteLine(edad);
+
+Console.WriteLine("-------------------------------");
+
+// Prefijo
+int a = 10;
+Console.WriteLine(a);
+Console.WriteLine(a++);
+Console.WriteLine(a);
+
+Console.WriteLine("b ->");
+
+int b = 10;
+Console.WriteLine(b);
+Console.WriteLine(++b);
+Console.WriteLine(b);
+
+//Prioridad
+//x++ x--
+//++x --x
+//* / %
+//+ -
+
+//Orden
+Console.WriteLine(2 + 2 * 2); //6
+Console.WriteLine((2 + 2) * 2); //8
+
+Console.WriteLine(9 / 5 / 2); //
+Console.WriteLine(9 / (5 / 2)); //4
