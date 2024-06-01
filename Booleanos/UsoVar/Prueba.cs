86406d2d025e355cfd7fdb3b3c6eef022ff5f4d3
miguegta <miguelgaitan4@gmail.com>
2024-05-31 19:09:27 -0600

    Fundamentos

---
+++ Booleanos/UsoVar/Prueba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoVar
+{
+    internal class Prueba
+    {
+        private void MetodoDos()
+        {
+        var nombre = "Migue";
+        }
+    }
+}
