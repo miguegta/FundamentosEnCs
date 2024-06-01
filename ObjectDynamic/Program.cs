75490fbb5aa66807b079eca026153d9d1a111e08
miguegta <miguelgaitan4@gmail.com>
2024-05-31 20:02:50 -0600

    Fundamentos

---
+++ ObjectDynamic/Program.cs
@@ -0,0 +1,16 @@
+﻿// ** OBJECT **
+object altura = 1.70;
+object nombre = "pedro";
+//int lo2 nombre.Length; <---- No se puede
+int lo3 = ((string)nombre).Length;
+Console.WriteLine(nombre + " tiene " + altura + " metros de alto.");
+
+//Uso de Length 
+string apellido = "Rosas";
+int lo = apellido.Length;
+Console.WriteLine(lo);
+Console.WriteLine(apellido + " tiene " + lo + " caracteres.");
+
+// ** DYNAMIC **
+dynamic algo = "Carrera";
+Console.WriteLine(algo.Length);
