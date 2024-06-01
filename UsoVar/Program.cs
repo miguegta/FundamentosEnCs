1fd2dc7f9dafd75b22ca98eea790512b107b4280
miguegta <miguelgaitan4@gmail.com>
2024-05-31 19:11:12 -0600

    Fundamentos

---
+++ UsoVar/Program.cs
@@ -0,0 +1,24 @@
+﻿//Crear una variable de forma explícita
+int edad = 20;
+
+//Uso de var
+var dato = "Hola";
+
+//Error
+var altura = 170.00;
+altura = 170;
+
+//Usos de var
+var numero1 = 10;
+var numero2 = 10.01;
+var numero3 = 20f;
+var numero4 = 20m;
+var texto1 = "hola";
+var texto2 = 'a';
+var alegre = true;
+
+void MetrodoPrueba()
+{
+    var holaMundo = "Mundo";
+}
+
